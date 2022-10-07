c447f1e6aaffa4cc78af2e1aed476ff7ddbef910
vimproved <vimproved@inventati.org>
2022-10-07 14:04:15 -0400

    complete neovim config

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,8 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.virtualenv ];
+  environment.systemPackages = with pkgs.vimPlugins; [
+    bufferline-nvim
+    lualine-nvim
+    coq_nvim
+    nord-nvim
+    nvim-tree-lua
+  ];
   programs.neovim = {
+    configure = {
+      customRC = ''
+        lua << EOF
+        ${builtins.readFile ./init.lua}
+        EOF
+      '';
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [ bufferline-nvim lualine-nvim coq_nvim nord-nvim nvim-tree-lua ];
+      };
+    };
     enable = true;
     defaultEditor = true;
     viAlias = true;
