ad74911bab39c32760eb0c924fc3fef5308e8603
vimproved <vimproved@inventati.org>
2022-10-07 11:41:57 -0400

    nixvim config

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -7,4 +7,44 @@
     viAlias = true;
     vimAlias = true;
   };
+  programs.nixvim = {
+    enable = true;
+    colorschemes.nord = {
+      enable = true;
+      borders = true;
+    };
+    plugins = {
+      bufferline.enable = true;
+      coq-nvim = {
+        enable = true;
+	autostart = "shut-up";
+	installArtifacts = true;
+      };
+      lsp = {
+        enable = true;
+	servers = {
+	  clangd.enable = true;
+	  eslint = true;
+	  pyright.enable = true;
+	  rnix-lsp.enable = true;
+	  rust-analyzer = true;
+	};
+	setupWrappers = [ ''require("coq").lsp_ensure_capabilities'' ];
+      };
+      lualine = {
+        enable = true;
+	extensions = [ "NvimTree" ];
+	theme = "Nord";
+      };
+      nvim-autopairs.enable = true;
+      nvim-tree = {
+        enable = true;
+	disableNetrw = true;
+	git.enable = true;
+	openOnSetup = true;
+	updateCwd = true;
+      };
+      treesitter.enable = true;
+    };
+  };
 }
